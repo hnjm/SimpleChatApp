ab766673b3001f5695b5c47090ee5fa8b4e3e092
eaktassssss <eaktasgithub@gmail.com>
2020-11-19 00:48:43 +0300

    Add project files.

---
+++ SignalR.Client/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SignalR.Client.Models;
+
+namespace SignalR.Client.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+       
+    }
+}
---
+++ SignalR.Server/Hubs/MessageHub.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SignalR.Server.Hubs
+{
+    public class MessageHub : Hub
+    {
+        public async Task SendMessage(string message)
+        {
+            try
+            {
+                if (String.IsNullOrEmpty(message))
+                    throw new ArgumentNullException(nameof(message));
+                else
+                {
+                    await Clients.All.SendAsync("ReceiveMessage", message);
+                }
+            }
+            catch (Exception exception)
+            {
+                throw new Exception(exception.Message);
+            }
+        }
+    }
+}
